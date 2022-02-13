388fd4e8e75b97bf63d977b02e7f07bc62b1e778
pshadikbasha <pshadikbasha97@gmail.com>
2022-02-13 22:45:24 +0530

    initial commit with node modules

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+class App extends React.component {
+  render() {
+    return (
+      <div>
+        <h4>Shadik reactjs</h4>
+      </div>
+    );
+  }
+}
+
+export default App;
